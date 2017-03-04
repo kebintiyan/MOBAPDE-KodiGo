09dcd7c10488cb4e18acaf320fe76e400c06f853
ryanroque17 <ryanroque17@gmail.com>
2017-03-05 03:46:11 +0800

    Add pages

--- KodiGo/app/src/main/java/ph/edu/dlsu/mobapde/chan_david_roque/kodigo/ViewNotebookActivity.java
+++ KodiGo/app/src/main/java/ph/edu/dlsu/mobapde/chan_david_roque/kodigo/ViewNotebookActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -15,9 +16,14 @@ public class ViewNotebookActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     FloatingActionButton addPageButton;
     PageAdapter pageAdapter;
-
-
+    View container;
+    DatabaseOpenHelper dbhelper;
+    Notebook currentNotebook;
+    ArrayList<Page> pages;
+    ArrayList<Integer> pagesID;
+    int currNotebookID;
     final static int REQUEST_ADD_PAGE = 0;
+    final static int REQUEST_EDIT_PAGE = 1;
     final static String KEY_PAGE = "page";
 
     @Override
@@ -25,17 +31,38 @@ public class ViewNotebookActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_notebook);
 
-        Page n = (Page) getIntent().getExtras().get(MainActivity.KEY_NOTEBOOK);
+        dbhelper = new DatabaseOpenHelper(getApplicationContext());
+        container = findViewById(R.id.activity_view_page);
+        currNotebookID = (int) getIntent().getExtras().get("currentNotebook");
+
+        currentNotebook = dbhelper.queryNotebookByID(currNotebookID);
+
         // Step 1: create recycler view
         recyclerView = (RecyclerView) findViewById(R.id.page_recyclerview);
 
-        ArrayList<Page> pages = new ArrayList<>();
+        pagesID = (ArrayList<Integer>) getIntent().getExtras().get("LoadedPages");
+        pages = new ArrayList<>();
+        for(int i=0; i<pagesID.size();i++){
+            Log.i("pageforID", ""+pagesID.get(i));
+            pages.add(dbhelper.queryPageByID(pagesID.get(i)));
+        }
 
+      //  ArrayList<Page> pages =  (ArrayList<Page>)
+        //pages.add((Page)getIntent().getExtras().get("editedPage"));
         // Step 3: Create our adapter
         pageAdapter = new PageAdapter(pages);
 
-        // Step 4: Attach adapter to UI
-        recyclerView.setAdapter(pageAdapter);
+        pageAdapter.setOnPageClickListener(new PageAdapter.OnPageClickListener() {
+            @Override
+            public void onPageClick(Page page) {
+                Intent i = new Intent(getBaseContext(), ViewPageActivity.class);
+                i.putExtra("Current Page", page.getPageID());
+                startActivityForResult(i,REQUEST_EDIT_PAGE);
+            }
+        });
+
+                // Step 4: Attach adapter to UI
+                recyclerView.setAdapter(pageAdapter);
 
         // Step 5: Attach layout manager to UI
         recyclerView.setLayoutManager(
@@ -46,9 +73,18 @@ public class ViewNotebookActivity extends AppCompatActivity {
         addPageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pageAdapter.addPage(new Page());
+                Log.i("newpage", "Page created");
+                int currentNotebookID = (int)getIntent().getExtras().get("currentNotebook");
+                int numOfPages = (int)getIntent().getExtras().get("numOfPages");
+                Intent i = new Intent(getBaseContext(), AddPageActivity.class);
+                Log.i("currNoteToadd", ""+currentNotebookID);
+                i.putExtra("currentNotebook", currentNotebookID);
+                i.putExtra("numOfPages", numOfPages);
+                startActivity(i);
             }
         });
+
+
 
         //addPageButton = (FloatingActionButton) findViewById(R.id.addPageButton);
 
@@ -63,8 +99,16 @@ public class ViewNotebookActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
         if(REQUEST_ADD_PAGE == requestCode && resultCode == RESULT_OK){
-            pageAdapter.addPage((Page) data.getExtras().get(KEY_PAGE));
+            String newPageID = data.getExtras().get("newPage").toString();
+            Page p = dbhelper.queryPageByID(Integer.parseInt(newPageID));
+            pageAdapter.addPage(p);
+        }else if(REQUEST_EDIT_PAGE == requestCode && resultCode == RESULT_OK){
+
+            Page p = dbhelper.queryPageByID((int) data.getExtras().get("editedPage"));
+            Log.i("GOTOEDIT", "PLS" +p.getName());
+            pageAdapter.editPage(p);
         }
     }
 }
