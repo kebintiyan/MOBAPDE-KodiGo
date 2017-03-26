7f509ae9526d5eef1de0e2df44b68cde88817d78
Kevin Chan <kevin98gray@gmail.com>
2017-03-26 17:49:31 +0800

    "Delete" comment

--- KodiGo/app/src/main/java/ph/edu/dlsu/mobapde/chan_david_roque/kodigo/CommentSpan.java
+++ KodiGo/app/src/main/java/ph/edu/dlsu/mobapde/chan_david_roque/kodigo/CommentSpan.java
@@ -18,19 +18,18 @@ public class CommentSpan extends ClickableSpan {
     private Comment comment;
     private String coveredText;
     private boolean isViewing;
+    private boolean deleted;
 
     public CommentSpan(Comment comment, String coveredText, boolean isViewing) {
         super();
         this.comment = comment;
         this.coveredText = coveredText;
         this.isViewing = isViewing;
+        this.deleted = false;
     }
 
     @Override
     public void onClick(final View widget) {
-        if (comment.getComment() == null)
-            return;
-
         if (isViewing) {
             new MaterialDialog.Builder(widget.getContext())
                     .title("Comment")
@@ -38,46 +37,56 @@ public class CommentSpan extends ClickableSpan {
                     .show();
         }
         else {
-            new MaterialDialog.Builder(widget.getContext())
-                    .title("Comment")
-                    .positiveText("Save")
-                    .negativeText("Delete")
-                    .input("Comment", comment.getComment(), false, new MaterialDialog.InputCallback() {
-                        @Override
-                        public void onInput(@NonNull MaterialDialog dialog, CharSequence input) {
-                            dialog.setContent(input);
-                            comment.setComment(input + "");
-                        }
-                    })
-                    .onNegative(new MaterialDialog.SingleButtonCallback() {
-                        @Override
-                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
-                            new MaterialDialog.Builder(widget.getContext())
-                                    .title("Delete?")
-                                    .content("Are you sure you want to delete this comment?")
-                                    .positiveText("Ok")
-                                    .negativeText("Cancel")
-                                    .onPositive(new MaterialDialog.SingleButtonCallback() {
-                                        @Override
-                                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
-                                            comment.setComment(null);
+            if (!deleted) {
+                new MaterialDialog.Builder(widget.getContext())
+                        .title("Comment")
+                        .positiveText("Save")
+                        .negativeText("Delete")
+                        .negativeColorRes(R.color.colorSecondaryText)
+                        .input("Comment", comment.getComment(), false, new MaterialDialog.InputCallback() {
+                            @Override
+                            public void onInput(@NonNull MaterialDialog dialog, CharSequence input) {
+                                dialog.setContent(input);
+                                comment.setComment(input + "");
+                            }
+                        })
+                        .onNegative(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                new MaterialDialog.Builder(widget.getContext())
+                                        .title("Delete?")
+                                        .content("Are you sure you want to delete this comment?" +
+                                        "\n Note: Changes will be saved on submit.")
+                                        .positiveText("Delete")
+                                        .positiveColorRes(R.color.colorMaterialRed)
+                                        .negativeText("Cancel")
+                                        .onPositive(new MaterialDialog.SingleButtonCallback() {
+                                            @Override
+                                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                                deleted = true;
 
-                                        }
-                                    })
-                                    .show();
-                        }
-                    })
-                    .show();
-        }
-    }
-
-    @Override
-    public void updateDrawState(TextPaint ds) {
-        super.updateDrawState(ds);
-
-        if (comment.getComment() == null) {
-            ds.setColor(Color.parseColor("#000000"));
-            ds.setUnderlineText(false);
+                                            }
+                                        })
+                                        .show();
+                            }
+                        })
+                        .show();
+            }
+            else {
+                new MaterialDialog.Builder(widget.getContext())
+                        .title("Deleted Comment")
+                        .titleColorRes(R.color.colorMaterialRed)
+                        .positiveText("Restore")
+                        .content(comment.getComment())
+                        .onPositive(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                deleted = false;
+                                CommentSpan.this.onClick(widget);
+                            }
+                        })
+                        .show();
+            }
         }
     }
 
